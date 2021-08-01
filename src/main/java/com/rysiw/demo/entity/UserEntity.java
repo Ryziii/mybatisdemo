96c54ba9ff0c78c01b955a7728f1918f885c24f3
Ryziii <az.convict@hotmail.com>
2021-08-01 18:12:49 +0800

    添加自动校验

--- src/main/java/com/rysiw/demo/entity/UserEntity.java
+++ src/main/java/com/rysiw/demo/entity/UserEntity.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 @AllArgsConstructor
 public class UserEntity {
     private Long id;
+
+    @NotEmpty
     private String username;
+
+    @NotEmpty
     private String password;
     private String phone;
     private String email;
